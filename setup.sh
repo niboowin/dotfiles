9e4f1f0f2fd40addcaf6ebf2938ce575e6dc33bb
Anthony Pilger <53822+niboowin@users.noreply.github.com>
2022-07-31 16:04:15 -0400

    Adding private stuff

--- setup.sh
+++ setup.sh
@@ -1,6 +1,8 @@
 ln -sf $PWD/.zshrc $HOME/.zshrc
 ln -sf $PWD/.vimrc $HOME/.vimrc
 ln -sf $PWD/.tmux.conf $HOME/.tmux.conf
-ln -sf $PWD/.gitconfig $HOME/.gitconfig
 ln -sf $PWD/.gitignore_global $HOME/.gitignore_global
 ln -sf $PWD/starship.toml $HOME/.config/starship.toml
+#Private Things..
+ln -sf $PWD/private/.gitconfig $HOME/.gitconfig
+
